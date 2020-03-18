725c5ebf89c27cd9e6d5453c8c3afe69002adf4e
Javier <vichango@gmail.com>
2020-03-18 12:27:26 +0100

    Générer automatiquement le nom du fichier par script.

--- regenerer-lsf-1.sh
+++ regenerer-lsf-1.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-grep ",LSF I," < Tout.csv > LSF-1.csv
+if [[ $# -eq 0 ]] ; then
+    echo 'The path to the CSV file is required'
+    exit 0
+fi
+
+grep ",LSF I," < "$1" > LSF-1.csv
 
 sed '1i\
-fr-mot,fr-desc,tags,lsf-desc,fss,elix,level,added,lsf-video,has-lsf-video
+fr_mot,fr_desc,tags,lsf_desc,fss,elix,level,added,has_lsf_video
 ' LSF-1.csv > LSF-1-header.csv
 
-mlr --csv cut -x -f level,added,has-lsf-video then \
-reorder -f fr-mot,fr-desc,lsf-video,lsf-desc,fss,elix,tags LSF-1-header.csv > LSF-1-sorted.csv
+mlr --csv \
+put '$lsf_video = "TRUE" == $has_lsf_video ? ("[sound:" . $fr_mot . ".mp4]") : ""' then \
+cut -x -f level,added,has_lsf_video then \
+reorder -f fr_mot,fr_desc,lsf_video,lsf_desc,fss,elix,tags LSF-1-header.csv > LSF-1-sorted.csv
 
 sed '1d' LSF-1-sorted.csv > LSF-1.csv
 
--- regenerer-lsf-2.sh
+++ regenerer-lsf-2.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-grep ",LSF II," < Tout.csv > LSF-2.csv
+if [[ $# -eq 0 ]] ; then
+    echo 'The path to the CSV file is required'
+    exit 0
+fi
+
+grep ",LSF II," < "$1" > LSF-2.csv
 
 sed '1i\
-fr-mot,fr-desc,tags,lsf-desc,fss,elix,level,added,lsf-video,has-lsf-video
+fr_mot,fr_desc,tags,lsf_desc,fss,elix,level,added,has_lsf_video
 ' LSF-2.csv > LSF-2-header.csv
 
-mlr --csv cut -x -f level,added,has-lsf-video then \
-reorder -f fr-mot,fr-desc,lsf-video,lsf-desc,fss,elix,tags LSF-2-header.csv > LSF-2-sorted.csv
+mlr --csv \
+put '$lsf_video = "TRUE" == $has_lsf_video ? ("[sound:" . $fr_mot . ".mp4]") : ""' then \
+cut -x -f level,added,has_lsf_video then \
+reorder -f fr_mot,fr_desc,lsf_video,lsf_desc,fss,elix,tags LSF-2-header.csv > LSF-2-sorted.csv
 
 sed '1d' LSF-2-sorted.csv > LSF-2.csv
 
