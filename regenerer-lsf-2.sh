ec6ff288499d791198cc494842928ecf56c35ead
Javier <vichango@gmail.com>
2020-03-18 13:11:00 +0100

    Add missing sort line in script and rename sorting column.

--- regenerer-lsf-2.sh
+++ regenerer-lsf-2.sh
@@ -13,8 +13,9 @@ fr_mot,fr_desc,tags,lsf_desc,fss,elix,level,added,has_lsf_video
 
 mlr --csv \
 put '$lsf_video = "TRUE" == $has_lsf_video ? ("[sound:" . $fr_mot . ".mp4]") : ""' then \
-put '$sort = tolower($fr_mot)' then \
-cut -x -f level,added,has_lsf_video,sort then \
+put '$fr_sort = tolower($fr_mot)' then \
+sort -f fr_sort then \
+cut -x -f level,added,has_lsf_video,fr_sort then \
 reorder -f fr_mot,fr_desc,lsf_video,lsf_desc,fss,elix,tags LSF-2-header.csv > LSF-2-sorted.csv
 
 sed '1d' LSF-2-sorted.csv > LSF-2.csv
