06c472ec64ddb6a8f1db777e82db5d7dc8dc3fdb
Javier <vichango@gmail.com>
2020-03-19 08:27:12 +0100

    Améliore l'aide pour les scripts.

--- regenerer-lsf-2.sh
+++ regenerer-lsf-2.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]] ; then
-    echo 'The path to the CSV file is required'
-    exit 0
+helpFunction()
+{
+   echo "Utilisé pour regénérer le fichier CSV à importer dans le Deck Anki « LSF II - Mots et formules » existant."
+   echo ""
+   echo "Utilisation: $0 -f <nom de fichier>"
+   echo -e "\t-f le <nom de fichier> du CSV à utiliser comme base"
+   exit 1 # Exit script after printing help.
+}
+
+while getopts "f:f" opt # The script doesn't work with a single "f".
+do
+   case "$opt" in
+      f ) filepath="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent.
+   esac
+done
+
+# Print helpFunction in case parameters are empty.
+if [ -z "$filepath" ]
+then
+   echo "Le nom du fichier est manquant ou vide.";
+   helpFunction
 fi
 
-grep ",LSF II," < "$1" > LSF-2.csv
+grep ",LSF II," < "$filepath" > LSF-2.csv
 
 sed '1i\
 fr_mot,fr_desc,tags,has_lsf_video,lsf_desc,fss,elix,level,added
@@ -24,3 +43,5 @@ rm LSF-2-header.csv
 rm LSF-2-sorted.csv
 
 mv LSF-2.csv listes/
+
+echo -e "Terminé!"
