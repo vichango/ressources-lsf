d28fcdd9425cc4ec1a6d6e11244c15976be367b2
Javier <vichango@gmail.com>
2020-03-19 08:33:32 +0100

    Fusionne les deux scripts de génération.

--- regenerer-listes.sh
+++ regenerer-listes.sh
@@ -2,14 +2,14 @@
 
 helpFunction()
 {
-   echo "Utilisé pour regénérer le fichier CSV à importer dans le Deck Anki « LSF II - Mots et formules » existant."
+   echo "Utilisé pour regénérer les fichiers CSV à importer dans les Decks Anki existants."
    echo ""
    echo "Utilisation: $0 -f <nom de fichier>"
    echo -e "\t-f le <nom de fichier> du CSV à utiliser comme base"
    exit 1 # Exit script after printing help.
 }
 
-while getopts "f:f" opt # The script doesn't work with a single "f".
+while getopts "f:f" opt # Hack, the script doesn't work with a single "f".
 do
    case "$opt" in
       f ) filepath="$OPTARG" ;;
@@ -24,6 +24,28 @@ then
    helpFunction
 fi
 
+# LSF I.
+grep ",LSF I," < "$filepath" > LSF-1.csv
+
+sed '1i\
+fr_mot,fr_desc,tags,has_lsf_video,lsf_desc,fss,elix,level,added
+' LSF-1.csv > LSF-1-header.csv
+
+mlr --csv \
+put '$lsf_video = "TRUE" == $has_lsf_video ? ("[sound:" . $fr_mot . ".mp4]") : ""' then \
+put '$fr_sort = tolower($fr_mot)' then \
+sort -f fr_sort then \
+cut -x -f level,added,has_lsf_video,fr_sort then \
+reorder -f fr_mot,fr_desc,lsf_video,lsf_desc,fss,elix,tags LSF-1-header.csv > LSF-1-sorted.csv
+
+sed '1d' LSF-1-sorted.csv > LSF-1.csv
+
+rm LSF-1-header.csv
+rm LSF-1-sorted.csv
+
+mv LSF-1.csv listes/
+
+# LSF II.
 grep ",LSF II," < "$filepath" > LSF-2.csv
 
 sed '1i\
