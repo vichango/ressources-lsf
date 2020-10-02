c07bed41bfe5e83a05ebef2a1dc9abf2d10425d6
Javier <vichango@gmail.com>
2020-10-02 15:03:23 +0200

    Mise à jour depuis Numbers

--- regenerer-liste.sh
+++ regenerer-liste.sh
@@ -40,6 +40,7 @@ put '$lsf_video_key = "TRUE" == $lsf_video_tmp ? $fr_mot : $lsf_video_tmp' then 
 put '$lsf_video = "FALSE" == $lsf_video_key ? "" : ("[sound:" . $lsf_video_key . ".mp4]")' then \
 put '$tags = "LSF I" == $level ? ("lsf-1 " . $tags) : $tags' then \
 put '$tags = "LSF II" == $level ? ("lsf-2 " . $tags) : $tags' then \
+put '$tags = "LSF III" == $level ? ("lsf-3 " . $tags) : $tags' then \
 put '$fr_sort = tolower($fr_mot)' then \
 sort -f fr_sort then \
 cut -x -f level,added,lsf_video_tmp,lsf_video_key,fr_sort then \
